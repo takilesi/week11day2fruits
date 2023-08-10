d94b1ea881b15eaa445c510bd6d95c290cb59f06
Doug <124453486+takilesi@users.noreply.github.com>
2023-08-10 11:09:01 -0400

    commit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 3000
 
 const fruits = require('./models/fruits.js');
 
+
+
+
+
+
 // -----MiddleWare 
 app.set("view engine", "jsx");
 
@@ -17,7 +22,37 @@ app.get('/fruits/', (req, res) => {
     res.send(fruits);
 });
 
-//add show route
+
+// USE 
+app.use(express.urlencoded({extended:false}));
+
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
+
+
+
+
+app.post('/fruits', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    res.send('data received');
+});
+
+// POST
+app.post('/fruits', (req, res) => {
+    res.send('hi');
+});
+
+
+//add SHOW route
 app.get('/fruits/:indexOfFruitsArray', (req, res) => {
     res.send(fruits);
 });
---
+++ views/New.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function New() {
+  return (
+    <div>
+        Form
+
+    </div>
+  )
+}
+
+export default New
